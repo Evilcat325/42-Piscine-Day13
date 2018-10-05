fa67016b8656ff529115cb1a28303a2bca26caf0
Sen Li <seli@e1z1r7p16.42.us.org>
2018-10-05 02:31:18 -0700

    cache result

--- ex05/btree_search_item.c
+++ ex05/btree_search_item.c
@@ -6,7 +6,7 @@
 /*   By: seli <seli@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/05 02:14:08 by seli              #+#    #+#             */
-/*   Updated: 2018/10/05 02:19:20 by seli             ###   ########.fr       */
+/*   Updated: 2018/10/05 02:31:01 by seli             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,18 @@
 void	*btree_search_item(t_btree *root,
 							void *data_ref, int (*cmpf)(void *, void *))
 {
+	t_btree	*left;
+	t_btree	*right;
+
 	if (!root)
 		return (0);
-	if (btree_search_item(root->left, data_ref, cmpf) != 0)
-		return (root->left);
-	else if (cmpf(root->item, data_ref))
+	left = btree_search_item(root->left, data_ref, cmpf);
+	if (left)
+		return (left);
+	if (cmpf(root->item, data_ref) == 0)
 		return (root);
-	else if (btree_search_item(root->right, data_ref, cmpf) != 0)
-		return (root->right);
+	right = btree_search_item(root->right, data_ref, cmpf);
+	if (right)
+		return (right);
 	return (0);
 }
